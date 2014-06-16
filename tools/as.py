cfb6165f260569dd3caacf5d053fef91f9f9f998
David Kaplan <dk@dk-ubuntu-14.04>
2014-06-16 11:02:09 +0300

    Assembler working for instructions with 2 operands

--- tools/as.py
+++ tools/as.py
@@ -1,18 +1,68 @@
-opcodes =
-        {
+opcodes = {
             'nop':0x80,     'mov':0x88,     'push':0x90,    'pop':0x98,
             'ld':0xa0,      'st':0xa8,      'b':0xb0,       'bne':0xb8,
             'eq':0x00,      'gt':0x08,      'lt':0x10,      'and':0x18,
             'or':0x20,      'not':0x28,     'xor':0x30,     'nor':0x38,
             'add':0x40,     'sub':0x48,     'inc':0x50,     'dec':0x58,
             'shl':0x60,     'shr':0x68
-        }
+          }
 
 registers = [ 'r0', 'r1' ]
 
-def __convert_assembly_ins(ins):
-    # mov r0, r1
-    op, operands = ins.split(' ', 1)
-    operand1, operand2 = operands.split(',')
-    operand2 = operand.lstrip()
+def __get_reg_value(reg):
+    if reg == 'r0': return 0
+    elif reg == 'r1': return 1
+    else: raise Exception("Invalid operand register")
 
+def __convert_assembly_ins(ins):
+    #print(ins)
+    # deal with comment
+    if ins.lstrip()[0] == ';':
+        return bytearray()
+
+    imm = None
+    addr = None
+
+    mach_code = bytearray(1)
+    
+    ins, operands = ins.split(' ', 1)
+    op1, op2 = operands.split(',')
+    op2 = op2.lstrip().strip()
+
+    # get opcode for ins
+    ins = opcodes[ins]
+
+    # op1 - always reg
+    ins |= __get_reg_value(op1)
+    
+    # 0p2 - reg/imm/addr
+    if op2[0] == '#':
+        ins |= 1 << 2;
+        imm = int(op2[1:])
+        if imm > 0xff:
+            raise Exception("Imm out of range")
+        mach_code.append(imm)
+    else:
+        ins |= __get_reg_value(op2) << 1
+
+    mach_code[0] = ins;
+
+    return mach_code
+
+if __name__ == "__main__":
+    import sys
+    args = sys.argv[1:]
+    
+    if len(args) < 2:
+        raise Exception('Invalid input/output files')
+
+    mach_code = bytearray()
+
+    with open(args[0], 'r') as f:
+        for l in f.readlines():
+            mach_code += __convert_assembly_ins(l)
+
+    with open(args[1], 'wb') as f:
+        f.write(mach_code)
+
+    print('{} -> {} - {} bytes'.format(args[0], args[1], len(mach_code)))
