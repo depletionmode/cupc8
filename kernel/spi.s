1195ce503f7dd251a14c80958d9cd773e0abe18a
DepletionMode <kaps@depletionmode.com>
2015-01-15 07:03:07 +0200

    spi driver fixes

--- kernel/spi.s
+++ kernel/spi.s
@@ -6,26 +6,26 @@
 %define _DEV3_ 4
 
 ; write byte (r0) - device 0
-spi_write_0:
+spi_write_DEV0_:
     st $f1_DEV0_0, r0  ;fill tx buffer
     mov r0, #1
     st $f1_DEV0_2, r0  ;transact
-wait_spi_write_0:
+wait_spi_write_DEV0_:
     ld r0, $f1_DEV0_3  ;read status
     eq r0, #1
-    bzf .end_spi_write
-    b .wait_spi_write
-end_spi_write_0:
+    bzf .end_spi_write_DEV0_
+    b .wait_spi_write_DEV0_
+end_spi_write_DEV0_:
     mov r0, #0
 
 ; read byte (r0) - device 0
-spi_read_0:
+spi_read_DEV0_:
     mov r0, #1
     st $f1_DEV0_2, r0  ;transact
-wait_spi_read_0:
+wait_spi_read_DEV0_:
     ld r0, $f1_DEV0_3  ;read status
     eq r0, #1
-    bzf .end_spi_read
-    b .wait_spi_read
-end_spi_read_0:
+    bzf .end_spi_read_DEV0_
+    b .wait_spi_read_DEV0_
+end_spi_read_DEV0_:
     ld r0, $f1_DEV0_1  ;read rx buffer
