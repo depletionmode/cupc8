81afa1c17daeb295eaeb0038d7673a030fbf5452
DepletionMode <kaps@depletionmode.com>
2015-01-16 14:14:14 +0200

    kernel - printing routines

--- kernel/kernel.s
+++ kernel/kernel.s
@@ -1,9 +1,12 @@
 ; kernel entry point
 main:
-    mov r0, #65    ; 'A'
     push pch
     push pcl
-    b .print_char
+    b .print_msg
+;    mov r0, #67    ; 'C'
+;    push pch
+;    push pcl
+;    b .print_char
 
     mov r0, #255
     st $f000, r0
