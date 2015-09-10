1a08d9208a566a09032fa880988ec23a69601959
David Kaplan <david@2of1.org>
2015-09-10 11:34:48 +0300

    uBASIC: token work

--- kernel/ubasic_tokenizer.s
+++ kernel/ubasic_tokenizer.s
@@ -110,8 +110,414 @@ ubasic_singlechar:
 	pop pcl
 	pop pch
 
+ubasic_isdigit_ptr:
+	xor r0, r0
+	ldd r0, [ub_ptr]
+	lt r0, #48
+	bzf .done
+	gt r0, #57
+	bzf .done
+	mov r0, #1
+	
+.done:
+	pop pcl
+	pop pch
+
+ubasic_set_nextptr_ptr_plus_val:
+	; helper fcn for ubasic_get_next_token
+	; r0: vlaue to add
+
+	ld r1, [ub_ptr]
+	add r1, r0
+	st [ub_nextptr], r1
+	; check wraparound
+	lt r1, r0
+	bzf .wraparound
+.nowraparound:
+	b .done
+.wraparound:
+	ld r1, [ub_ptr+1]
+	add r1, #1
+	st [ub_nextptr+1], r1
+
+.done:
+	pop pcl
+	pop pch
+
+ub_s_let db "let"
+ub_s_print db "print"
+ub_s_if db "if"
+ub_s_then db "then"
+ub_s_else db "else"
+ub_s_for db "for"
+ub_s_to db "to"
+ub_s_next db "next"
+ub_s_goto db "goto"
+ub_s_gosub db "gosub"
+ub_s_return db "return"
+ub_s_call db "call"
+us_s_rem db "rem"
+ub_s_peek db "peek"
+ub_s_poke db "poke"
+ub_s_end db "end"
 ubasic_get_next_token:
 	;todo
+
+	ld r0, [ub_ptr]
+	gt r0, #0
+	bzf .next
+	mov r0, TOKENIZER_ENDOFINPUT
+	b .done
+
+.if0:
+	push pch
+	push pcl
+	b ubasic_isdigit_ptr
+	eq r0, #0
+	bzf .elseif0_0
+
+	xor r1, r1 ; r1 := i
+.loop:
+	gt r1, #5
+	bzf .error
+.if1:
+	ldd r0, [ub_ptr]+r1
+	push r1
+	push pch
+	push pcl
+	b ubasic_isdigit
+	pop r1
+	gt r0, #0
+	bzf .else1
+.if2:
+	eq r1, #0
+	bzf .else2
+	ld r1, [ub_ptr]
+	ld r0, [ub_ptr+1]
+	add r1, #1
+	gt r1, #0
+	bzf .nocarry0
+	add r0, #1
+.nocarry0:
+	st [ub_nextptr], r1
+	st [ub_nextptr+1], r0
+	mov r0, TOKENIZER_NUMBER
+	b .done
+.else2:
+	b .error
+.else1:
+	add r1, #1
+	push r1
+	b .loop	
+
+.elseif0_0:
+	push pch
+	push pcl
+	b ubasic_singlechar
+	push r0
+	eq r0, #0
+	bzf .elseif0_1
+	ld r1, [ub_ptr]
+	ld r0, [ub_ptr+1]
+	add r1, #1
+	gt r1, #0
+	bzf .nocarry1
+	add r0, #1
+.nocarry1:
+	st [ub_nextptr], r1
+	st [ub_nextptr+1], r0
+	pop r0
+	b .done
+	
+.elseif0_1:
+	ldd r0, [ub_ptr]
+	eq r0, #34
+	bzf .true0_1
+	b .else0	
+.true0_1:
+	ld r1, [ub_ptr]
+	ld r0, [ub_ptr+1]
+	st [ub_nextptr], r1
+	st [ub_nextptr+1], r0
+.loop1:
+	ld r1, [ub_nextptr]
+	ld r0, [ub_nextptr+1]
+	add r1, #1
+	gt r1, #0
+	bzf .nocarry2
+	add r0, #1
+.nocarry2:
+	st [ub_nextptr], r1
+	st [ub_nextptr+1], r0
+	ldd r0, [ub_nextptr]
+	eq r0, #34
+	bzf .loop1_end
+	b .loop1
+.loop1_end:
+	ld r1, [ub_nextptr]
+	ld r0, [ub_nextptr+1]
+	add r1, #1
+	gt r1, #0
+	bzf .nocarry3
+	add r0, #1
+.nocarry3:
+	st [ub_nextptr], r1
+	st [ub_nextptr+1], r0
+	mov r0, TOKENIZER_STRING
+	b .done
+	
+.else0:
+	mov r0, #>[ub_ptr]
+	mov r1, #<[ub_ptr]
+	push pch
+	push pcl
+	b str_cmp_set
+
+	; todo string matches
+	mov r0, #>[ub_s_let]
+	mov r1, #<[ub_s_let]
+	push pch
+	push pcl
+	b str_cmp
+	gt r0, #0
+	bzf .n0
+	mov r0, #3	; strlen
+	push pch
+	push pcl
+	b ubasic_set_nextptr_ptr_plus_val
+	mov r0, TOKENIZER_LET
+	b .done
+.n0:
+	mov r0, #>[ub_s_print]
+	mov r1, #<[ub_s_print]
+	push pch
+	push pcl
+	b str_cmp
+	gt r0, #0
+	bzf .n1
+	mov r0, #5	; strlen
+	push pch
+	push pcl
+	b ubasic_set_nextptr_ptr_plus_val
+	mov r0, TOKENIZER_PRINT
+	b .done
+.n1:
+	mov r0, #>[ub_s_if]
+	mov r1, #<[ub_s_if]
+	push pch
+	push pcl
+	b str_cmp
+	gt r0, #0
+	bzf .n2
+	mov r0, #2	; strlen
+	push pch
+	push pcl
+	b ubasic_set_nextptr_ptr_plus_val
+	mov r0, TOKENIZER_IF
+	b .done
+.n2:
+	mov r0, #>[ub_s_then]
+	mov r1, #<[ub_s_then]
+	push pch
+	push pcl
+	b str_cmp
+	gt r0, #0
+	bzf .n3
+	mov r0, #4	; strlen
+	push pch
+	push pcl
+	b ubasic_set_nextptr_ptr_plus_val
+	mov r0, TOKENIZER_THEN
+	b .done
+.n3:
+	mov r0, #>[ub_s_else]
+	mov r1, #<[ub_s_else]
+	push pch
+	push pcl
+	b str_cmp
+	gt r0, #0
+	bzf .n4
+	mov r0, #4	; strlen
+	push pch
+	push pcl
+	b ubasic_set_nextptr_ptr_plus_val
+	mov r0, TOKENIZER_ELSE
+	b .done
+.n4:
+	mov r0, #>[ub_s_for]
+	mov r1, #<[ub_s_for]
+	push pch
+	push pcl
+	b str_cmp
+	gt r0, #0
+	bzf .n5
+	mov r0, #3	; strlen
+	push pch
+	push pcl
+	b ubasic_set_nextptr_ptr_plus_val
+	mov r0, TOKENIZER_FOR
+	b .done
+.n5:
+	mov r0, #>[ub_s_to]
+	mov r1, #<[ub_s_to]
+	push pch
+	push pcl
+	b str_cmp
+	gt r0, #0
+	bzf .n6
+	mov r0, #2	; strlen
+	push pch
+	push pcl
+	b ubasic_set_nextptr_ptr_plus_val
+	mov r0, TOKENIZER_TO
+	b .done
+.n6:
+	mov r0, #>[ub_s_next]
+	mov r1, #<[ub_s_next]
+	push pch
+	push pcl
+	b str_cmp
+	gt r0, #0
+	bzf .n7
+	mov r0, #4	; strlen
+	push pch
+	push pcl
+	b ubasic_set_nextptr_ptr_plus_val
+	mov r0, TOKENIZER_NEXT
+	b .done
+.n7:
+	mov r0, #>[ub_s_goto]
+	mov r1, #<[ub_s_goto]
+	push pch
+	push pcl
+	b str_cmp
+	gt r0, #0
+	bzf .n8
+	mov r0, #4	; strlen
+	push pch
+	push pcl
+	b ubasic_set_nextptr_ptr_plus_val
+	mov r0, TOKENIZER_GOTO
+	b .done
+.n8:
+	mov r0, #>[ub_s_godub]
+	mov r1, #<[ub_s_gosub]
+	push pch
+	push pcl
+	b str_cmp
+	gt r0, #0
+	bzf .n9
+	mov r0, #5	; strlen
+	push pch
+	push pcl
+	b ubasic_set_nextptr_ptr_plus_val
+	mov r0, TOKENIZER_GOSUB
+	b .done
+.n9:
+	mov r0, #>[ub_s_return]
+	mov r1, #<[ub_s_return]
+	push pch
+	push pcl
+	b str_cmp
+	gt r0, #0
+	bzf .n10
+	mov r0, #6	; strlen
+	push pch
+	push pcl
+	b ubasic_set_nextptr_ptr_plus_val
+	mov r0, TOKENIZER_RETURN
+	b .done
+.n10:
+	mov r0, #>[ub_s_call]
+	mov r1, #<[ub_s_call]
+	push pch
+	push pcl
+	b str_cmp
+	gt r0, #0
+	bzf .n11
+	mov r0, #4	; strlen
+	push pch
+	push pcl
+	b ubasic_set_nextptr_ptr_plus_val
+	mov r0, TOKENIZER_CALL
+	b .done
+.n11:
+	mov r0, #>[ub_s_rem]
+	mov r1, #<[ub_s_rem]
+	push pch
+	push pcl
+	b str_cmp
+	gt r0, #0
+	bzf .n12
+	mov r0, #3	; strlen
+	push pch
+	push pcl
+	b ubasic_set_nextptr_ptr_plus_val
+	mov r0, TOKENIZER_REM
+	b .done
+.n12:
+	mov r0, #>[ub_s_peek]
+	mov r1, #<[ub_s_peek]
+	push pch
+	push pcl
+	b str_cmp
+	gt r0, #0
+	bzf .n13
+	mov r0, #4	; strlen
+	push pch
+	push pcl
+	b ubasic_set_nextptr_ptr_plus_val
+	mov r0, TOKENIZER_PEEK
+	b .done
+.n13:
+	mov r0, #>[ub_s_poke]
+	mov r1, #<[ub_s_poke]
+	push pch
+	push pcl
+	b str_cmp
+	gt r0, #0
+	bzf .n14
+	mov r0, #4	; strlen
+	push pch
+	push pcl
+	b ubasic_set_nextptr_ptr_plus_val
+	mov r0, TOKENIZER_POKE
+	b .done
+.n14:
+	mov r0, #>[ub_s_end]
+	mov r1, #<[ub_s_end]
+	push pch
+	push pcl
+	b str_cmp
+	gt r0, #0
+	bzf .error
+	mov r0, #3	; strlen
+	push pch
+	push pcl
+	b ubasic_set_nextptr_ptr_plus_val
+	mov r0, TOKENIZER_END
+	b .done
+
+.variable:
+	ldd r0, [ub_ptr]
+	lt r0, #97
+	bzf .error
+	gt r0, #122
+	bzf .error
+	ld r1, [ub_ptr]
+	ld r0, [ub_ptr+1]
+	add r1, #1
+	gt r1, #0
+	bzf .nocarry
+	add r0, #1
+.nocarry:
+	st [ub_nextptr], r1
+	st [ub_nextptr+1], r0
+	mov r0, TOKENIZER_VARIABLE
+	b .done
+	
+.error:
+	mov r0, TOKENIZER_ERROR	
 
 .done:
 	pop pcl
@@ -174,13 +580,11 @@ ubasic_tokenizer_next:
 	ld r1, [ub_ptr]
 	ld r0, [ub_ptr+1]
 	add r1, #1
-	eq r1, #0
-	bzf .carry0
-	b .aftercarry0
-.carry0:
+	gt r1, #0
+	bzf .nocarry
 	add r0, #1
 	st [ub_ptr+1], r0
-.aftercarry0:
+.nocarry:
 	st [ub_ptr], r1
 
 .n0:
