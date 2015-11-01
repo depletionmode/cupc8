4a467399d200577afb9b8e88a79817353a08a032
David Kaplan <davidka@il.ibm.com>
2015-11-01 11:30:40 +0200

    Kernel: modified math_mul()

--- kernel/math.s
+++ kernel/math.s
@@ -1,27 +1,4 @@
 
-mul_n: resb 1
-mul_v: resb 1
-math_mul:
-    ; r0 = r0 * r1
-    st [mul_v], r0
-    st [mul_n], r1
-    mov r1, #1  ; i=1
-.loop:
-    push r0
-    ld r0, [mul_n]
-    eq r0, r1
-    pop r0
-    bzf .done  ; i==mul_n ? mul_done
-    push r1
-    ld r1, [mul_v]
-    add r0, r1
-    pop r1
-    add r1, #1  ; i++
-    b .loop
-.done:
-    pop pcl
-    pop pch
-
 div_q: resb 1
 math_div:
 	; r0 = r0 / r1
@@ -44,3 +21,23 @@ math_div:
 	ld r0, [div_q]
 	pop pcl
 	pop pch
+
+mul_n: resb 1
+math_mul:
+	st [mul_n], r0
+	xor r0, r0
+.loop:
+	push r0
+	ld r0, [mul_n]
+	eq r0, #0
+	bzf .done
+	sub r0, #1
+	st [mul_n], r0
+	pop r0
+	add r0, r1
+	b .loop
+.done:
+	pop r0
+	pop pcl
+	pop pch
+	
