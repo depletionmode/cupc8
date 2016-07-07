0408f15b926676585de3bf4b1a84e48867de39c3
David Kaplan <david@2of1.org>
2016-07-07 16:00:54 +0300

    soc: compile scripts

---
+++ soc/compile.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/mnt/storage_hdd_red/quartus/13.1/quartus/bin/quartus_sh --flow compile soc
---
+++ soc/program.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/mnt/storage_hdd_red/quartus/13.1/quartus/bin/quartus_pgm -c USB-Blaster -m JTAG -o 'P;output_files/soc.sof'
